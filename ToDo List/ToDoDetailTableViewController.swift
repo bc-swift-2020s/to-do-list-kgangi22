e89fd0b7faadf48a4b72d86725780ab100f5310a
Kyle Gangi <gangiky@bc.edu>
2020-02-08 20:40:22 -0500

    Section 5.6 Completed

--- ToDo List/ToDoDetailTableViewController.swift
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -9,29 +9,32 @@
 import UIKit
 
 class ToDoDetailTableViewController: UITableViewController {
-
+    
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var noteView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
     
-    var toDoItem: String!
+    var toDoItem: ToDoItem!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if toDoItem == nil{
-            toDoItem = ""
+            toDoItem = ToDoItem(name: "", date: Date(), notes: "")
         }
         
         
-        nameField.text = toDoItem
+        nameField.text = toDoItem.name
+        datePicker.date = toDoItem.date
+        noteView.text = toDoItem.notes
 
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        toDoItem = nameField.text
+        toDoItem = ToDoItem(name: nameField.text!, date: datePicker.date, notes: noteView.text)
+        
     }
     
     
---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Kyle Gangi on 2/8/20.
+//  Copyright © 2020 Kyle Gangi. All rights reserved.
+//
+
+import Foundation
+
+
+struct ToDoItem {
+       var name: String
+       var date: Date
+       var notes: String
+   }
